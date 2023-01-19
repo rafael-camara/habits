a81fd51cc06e50e2a0f6374d3d480b7ca4341837
Rafael Câmara <rafaelcamara.377@gmail.com>
2023-01-19 12:05:23 -0300

    created tables, db seed and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,14 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
-app.register(cors)
+app.register(cors);
+app.register(appRoutes);
 
-const port = 3333
-
-app.get("/hello", async () => {
-  const habits = await prisma.habit.findMany()
-
-  return habits
-});
+const port = 3333;
 
 app.listen({ port }).then(() => {
-  console.log('HTTP Server running! | PORT:', port)
-})
+  console.log("HTTP Server running! | PORT:", port);
+});
