948c6f4d0f5895898bd952df2bb93bf307480c17
Rafael Câmara <rafaelcamara.377@gmail.com>
2023-01-22 22:27:09 -0300

    update files server and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,20 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+
 import { appRoutes } from "./routes";
 
 const app = Fastify();
 
-app.register(cors);
+app.register(cors, {
+  origin: true,
+});
 app.register(appRoutes);
 
-const port = 3333;
-
-app.listen({ port }).then(() => {
-  console.log("HTTP Server running! | PORT:", port);
-});
+app
+  .listen({
+    port: 3333,
+    host: "0.0.0.0",
+  })
+  .then((url) => {
+    console.log(`HTTP Server running! | PORT: ${url}`);
+  });
